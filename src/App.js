ae7dd017cb2d333c4f1741b04ddc3d677cbb6dd4
Zakir Hussain <zhussain202@gmail.com>
2023-10-04 19:35:05 +0530

    local

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { Fragment, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    // console.log(useState("hus"));
+
+    const [count,setCount] = useState(0);
+
+    function increment(){
+        // count = count + 1;
+        setCount(count + 1)
+        setCount(count + 1)
+        setCount(count + 1)
+    }
+
+    return (
+        <Fragment>
+        <div>{count}</div>
+        <button onClick={increment}>
+            Click Alert
+        </button>
+        </Fragment>
+    );
 }
 
 export default App;
